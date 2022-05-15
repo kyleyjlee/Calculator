9cbc1dd535a690d86dea3951b9fce9e13ee5873d
kyleyjlee <105597769+kyleyjlee@users.noreply.github.com>
2022-05-16 00:36:11 +0900

    Create 사칙연산.py

---
+++ 사칙연산.py
@@ -0,0 +1,12 @@
+a = int(input("숫자:"))
+b = int(input("숫자:"))
+c = (input("+,-,*,/ 쓰고싶은 기호는? :"))
+
+if c == "+" :
+  print(a+b)
+if c == "-" :
+  print(a-b)
+if c == "*" :
+  print(a*b)
+if c == "/" :
+  print(a/b)
