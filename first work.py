f59b4dd7aa81304570f2fa31be465f3dcf09ff5c
kyleyjlee <105597769+kyleyjlee@users.noreply.github.com>
2022-05-16 00:20:00 +0900

    Create first work.py

---
+++ first work.py
@@ -0,0 +1,12 @@
+a = int(input("숫자:"))
+b = int(input("숫자:"))
+c = int(input("두개를 곱한값은? :"))
+d =  int(a*b)
+
+if a * b > c:
+    print("wrong")
+elif a * b < c:
+    print("wrong")
+elif a * b == c :
+ print("correct")
+print(d)
